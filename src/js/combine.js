a6a1a5c5739058d782460dc38e7bb4c7d69b163d
Aditya30307 <anukhajuria089@gmail.com>
2025-04-17 23:23:16 +0530

    my first commit

---
+++ src/js/combine.js
@@ -0,0 +1,8 @@
+// app.js
+import { logic } from './script.js';  // Import initializeSearch() from script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize the 3D model and start the animation
+  // Initialize the search-related functionality
+  logic();
+});
